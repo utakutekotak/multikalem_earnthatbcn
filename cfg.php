8ad16db5401be1951e7f6f3946254cf561de3b52
utakutekotak <94692462+utakutekotak@users.noreply.github.com>
2021-11-19 21:09:51 +0700

    Add files via upload
    Edit Lebih dulu cfg.php dengan data dari websitenya hatetepes:///earnthatbcn[dot]getthatcrypto[dot]online

---
+++ cfg.php
@@ -0,0 +1,7 @@
+<?php
+
+#USER_AGENT
+$useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36";
+
+#COOKIE_(AMBIL COOKIE YANG TERAKHIR KALIAN CAPTURE)
+$cookie = "PHPSESSID=ed1394f18def8e647706725aa0d7cc8b; DGBToken=asc7bBNMkyLRpm5n8tWW71juPbhz3yWK; XRPToken=hOHlLUtUXLV3ZdLg8zN5UuL8kDldloyJ";
